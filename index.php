e6923ff5a9c5e9735c2108a893d097a99a5cd8aa
Unknown <budiebasri@gmail.com>
2017-10-31 05:33:44 +0800

    First Commit
    First commit for Coffee Picker Web Application

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Coffee Picker</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/cp-style.css">
+	<script src="lib/jquery-3.2.1.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 login-box center">
+			<div class="form-group logo-div">
+				<img src="img/cp-logo.png" class="img-circle logo" alt="Coffee Picker logo">
+			</div>
+			<div class="form-group">
+				<h3 class="center">COFFEE PICKER</h3>
+			</div>
+			<div class="form-group">
+				<label for="login-username">Username</label>
+				<input type="text" class="form-control" id="login-username">
+			</div>
+			<div class="form-group">
+				<label for="login-password">Password</label>
+				<input type="password" class="form-control" id="login-password">
+			</div>
+			<div class="form-group">
+				<button type="button" class="btn btn-default btn-block" onclick="location.href = 'view/admindashboard.php'">LOGIN</button>
+			</div>
+
+
+		</div>
+
+	</div>
+
+</body>
+</html>
---
+++ view/admindashboard.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Coffee Picker | Admin Dashboard</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../lib/font-awesome-4.7.0/css/font-awesome.css">
+
+	<link rel="stylesheet" type="text/css" href="../css/sidemenu.css">
+	<link rel="stylesheet" type="text/css" href="../css/cp-style.css">
+
+	<script src="../lib/jquery-3.2.1.min.js"></script>
+	<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+	<nav  class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="navbar-header navbar-right toright"> 
+			<span class="navbar-brand white" style="margin-right: 20px;" href="#">Welcome Administrator</span>
+			<span class="navbar-brand white" style="margin-right: 20px;" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i></span>
+		</div>
+
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-left mobileright">
+				<li> <a class="white" href="admindashboard.php">COFFEE PICKER</a></li>
+			</ul>
+
+		</div>
+
+	</nav>
+
+
+	<?php
+	include 'admin/sidemenu.php';
+	?>
+
+	<div class="container">
+
+		<div class="row">
+		  <div class="col-xs-6 col-md-3">
+		    <a href="#" class="thumbnail">
+		      
+		    </a>
+		  </div>
+		</div>
+
+
+
+	</div>
+
+</body>
+</html>
