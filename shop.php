f9dd39a40a345e52273d531fe7d44958def29141
Muhamad Budie <budiebasri@gmail.com>
2017-11-20 13:21:32 +0800

    add shop search list
    lol

--- shop.php
+++ shop.php
@@ -5,12 +5,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="lib/font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="lib/datatables/datatables.css">
 	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/cp-style.css">
   <link rel="stylesheet" type="text/css" href="css/half-slider.css">
 
 	<script src="lib/jquery-3.2.1.min.js"></script>
 	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="lib/datatables/datatables.js"></script>
 
 </head>
 <body class="">
@@ -62,8 +64,35 @@
 	  <div class="panel panel-default">
 	   <div class="panel-heading">Search Filter</div>
 	    <div class="panel-body">
-				<h4>By State</h4>
-				<h4>By Country</h4>
+				<form>
+				  <div class="form-group">
+				    <label for="country">Country:</label>
+				    <input type="text" class="form-control" id="country_search" value="Malaysia">
+				  </div>
+				  <div class="form-group">
+				    <label for="state">State:</label>
+				    <select class="form-control" id="state_search">
+							<option>Melaka</option>
+							<option>Johor</option>
+							<option>Kedah</option>
+							<option>N.Sembilan</option>
+							<option>Kuala Lumpur</option>
+							<option>Pahang</option>
+						</select>
+				  </div>
+					<div class="form-group">
+				    <label for="pwd">Select Area:</label>
+						<select class="form-control" id="area_search">
+							<option>Melaka</option>
+							<option>Johor</option>
+							<option>Kedah</option>
+							<option>N.Sembilan</option>
+							<option>Kuala Lumpur</option>
+							<option>Pahang</option>
+						</select>
+				  </div>
+				  <button type="submit" class="btn btn-default">Search</button>
+				</form>
 	   </div>
 	  </div>
 	 </div>
@@ -72,7 +101,110 @@
 		 <div class="panel-heading">List of Shop</div>
 			<div class="panel-body">
 				<input type="text" class="form-control" id="" placeholder="Search your shop here">
-				<h4>using Data table</h4>
+
+				<div class="row">
+						<div class="[ col-xs-12 col-sm-offset-2 col-sm-8 ]">
+							<ul class="shop-list-list">
+								<li>
+									<time datetime="2014-07-20">
+										<span class="day">4</span>
+										<span class="month">Jul</span>
+										<span class="year">2014</span>
+										<span class="time">ALL DAY</span>
+									</time>
+									<img alt="Independence Day" src="https://farm4.staticflickr.com/3100/2693171833_3545fb852c_q.jpg" />
+									<div class="info">
+										<h2 class="title">Independence Day</h2>
+										<p class="desc">United States Holiday</p>
+									</div>
+									<div class="social">
+										<ul>
+											<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+											<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+											<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+										</ul>
+									</div>
+								</li>
+
+								<li>
+									<time datetime="2014-07-20 0000">
+										<span class="day">8</span>
+										<span class="month">Jul</span>
+										<span class="year">2014</span>
+										<span class="time">12:00 AM</span>
+									</time>
+									<div class="info">
+										<h2 class="title">One Piece Unlimited World Red</h2>
+										<p class="desc">PS Vita</p>
+										<ul>
+											<li style="width:50%;"><a href="#website"><span class="fa fa-globe"></span> Website</a></li>
+											<li style="width:50%;"><span class="fa fa-money"></span> $39.99</li>
+										</ul>
+									</div>
+									<div class="social">
+										<ul>
+											<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+											<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+											<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+										</ul>
+									</div>
+								</li>
+
+								<li>
+									<time datetime="2014-07-20 2000">
+										<span class="day">20</span>
+										<span class="month">Jan</span>
+										<span class="year">2014</span>
+										<span class="time">8:00 PM</span>
+									</time>
+									<img alt="My 24th Birthday!" src="https://farm5.staticflickr.com/4150/5045502202_1d867c8a41_q.jpg" />
+									<div class="info">
+										<h2 class="title">Mouse0270's 24th Birthday!</h2>
+										<p class="desc">Bar Hopping in Erie, Pa.</p>
+										<ul>
+											<li style="width:33%;">1 <span class="glyphicon glyphicon-ok"></span></li>
+											<li style="width:34%;">3 <span class="fa fa-question"></span></li>
+											<li style="width:33%;">103 <span class="fa fa-envelope"></span></li>
+										</ul>
+									</div>
+									<div class="social">
+										<ul>
+											<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+											<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+											<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+										</ul>
+									</div>
+								</li>
+
+								<li>
+									<time datetime="2014-07-31 1600">
+										<span class="day">31</span>
+										<span class="month">Jan</span>
+										<span class="year">2014</span>
+										<span class="time">4:00 PM</span>
+									</time>
+									<img alt="Disney Junior Live On Tour!" src="http://www.thechaifetzarena.com/images/main/DL13_PiratePrincess_thumb.jpg" />
+									<div class="info">
+										<h2 class="title">Disney Junior Live On Tour!</h2>
+										<p class="desc"> Pirate and Princess Adventure</p>
+										<ul>
+											<li style="width:33%;">$49.99 <span class="fa fa-male"></span></li>
+											<li style="width:34%;">$29.99 <span class="fa fa-child"></span></li>
+										</ul>
+									</div>
+									<div class="social">
+										<ul>
+											<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+											<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+											<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+
 		 </div>
 		</div>
 	 </div>
